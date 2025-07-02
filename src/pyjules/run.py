44b79a6391ce1fb537dc48b29c79d0caa74e2236
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-07-02 11:34:00 +0100

    add docstrings and some checks

--- src/pyjules/run.py
+++ src/pyjules/run.py
@@ -1,15 +1,61 @@
+from os import PathLike
 import pathlib
 import subprocess
 
-def run(working_dir: str, config_dir: str | None = None, container_name: str = "jules") -> None:
+class InvalidPath(Exception):
+    pass
+
+class InvalidName(Exception):
+    pass
+
+def run(run_dir: str | PathLike, namelists_dir: str | PathLike | None = None, container_name: str = "JULES") -> None:
     """
-    Run Jules.
+    Run a containerised version of JULES.
+
+    Must run `pyjules.setup` first.
+
+    Args:
+      run_dir: Path to the directory in which the jules executable will be run.
+      namelists_dir: Path to the directory containing the namelists.
+      container_name: The name of the container to be run.
     """
+    # Check valid name (possibly overkill)
+    try: 
+        subprocess.run(
+            ["udocker", "inspect", container_name],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise InvalidName(exc.stderr) from exc
 
     cwd = pathlib.Path.cwd()
-    working_dir = pathlib.Path(working_dir).resolve().relative_to(cwd)
-    config_dir = pathlib.Path(config_dir).resolve().relative_to(cwd)
+    run_dir = Path(run_dir).resolve()
+    namelists_dir = run_dir if namelists_dir is None else Path(namelists_dir).resolve()
+
+    # We will mount the cwd when running the container. Hence, run_dir and
+    # namelists_dir must be subdirectories of cwd!
+    if not (run_dir.is_relative_to(cwd) and namelists_dir.is_relative_to(cwd)):
+        msg = f"Both `run_dir` and `namelists_dir` must be subdirectories of the current working directory, {cwd}!"
+        raise InvalidPath(msg)
+    
+    run_dir = run_dir.relative_to(cwd)
+    namelists_dir = namelists_dir.relative_to(cwd)
+
+    # This is where the cwd will end up in the container filesystem
+    mount_point = pathlib.Path("/run")
 
     subprocess.run(
-        ["udocker", "run", "-v", f"{cwd}:/run", container_name, "-d", f"/run/{working_dir}" , f"/run/{config_dir}"],
+        [
+            "udocker",
+            "run",
+            "-v",
+            f"{cwd}:{mount_point}",
+            container_name,
+            "-d",
+            mount_point / working_dir,
+            mount_point / namelists_dir
+        ],
     )
--- src/pyjules/setup.py
+++ src/pyjules/setup.py
@@ -1,12 +1,19 @@
+from os import PathLike
 import subprocess
 
-def setup(image_path: str, image_name: str = "jules") -> None:
+def setup(image_file: str | PathLike, name: str = "JULES") -> None:
     """
-    One-time setup for running Jules.
+    Perform a one-time setup for running dockerised JULES.
 
-    1. Run `udocker install`
-    2. Load container from .tar file
-    3. Verify loaded container
+    This function goes through the following steps:
+
+    1. `udocker install` to set up udocker.
+    2. `udocker load` to load an image from `image_file`.
+    3. `udocker verify` to check the loaded image isn't corrupted.
+
+    Args:
+      image_file: A `.tar` containing the image, created using `docker save`.
+      name: A name for the image.
     """
     # NOTE: could use udocker python API directly
     # TODO: handle stdout/stderr
@@ -16,7 +23,7 @@ def setup(image_path: str, image_name: str = "jules") -> None:
     )
 
     subprocess.run(
-        ["udocker", "--allow-root", "load", "-i", image_path, image_name],
+        ["udocker", "--allow-root", "load", "-i", image_file, name],
     )
 
     subprocess.run(
